942882116bf3198641da4b9f1faeccae2d6a583e
Matheus Souza <matheusouzaaaa@gmail.com>
2021-09-02 22:06:14 -0300

    Montagem do ambiente para a realização da sprint 1

---
+++ app/src/main/java/com/example/tcc/EstudioDetalheActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tcc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EstudioDetalheActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_estudio_detalhe)
+    }
+}
---
+++ app/src/main/java/com/example/tcc/adapter/EstudioAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.tcc.adapter
+
+class EstudioAdapter {
+}
